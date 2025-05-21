429b0fc8b439faed728c2845d3335ec3a2d485b0
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-05-21 22:39:30 +0800

    feat: implement file output

--- quiz_creator/quiz_writer.py
+++ quiz_creator/quiz_writer.py
@@ -1,3 +1,4 @@
 class QuizWriter:
-    def write_to_file():
-        pass
+    def write_to_file(self, formatted_quiz, filename="quiz_output.txt"):
+        with open(filename, "a", encoding="utf-8") as file:
+            file.write(formatted_quiz)
