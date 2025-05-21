f872bf4f1c689db8f54b8a66cfb7ce959fa65c13
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-05-22 00:24:19 +0800

    feat: run full quiz from main

--- quiz_game/game_main.py
+++ quiz_game/game_main.py
@@ -2,7 +2,11 @@ from quiz_loader import QuizLoader
 from quiz_runner import QuizRunner
 
 def main():
-    pass
+    loader = QuizLoader()
+    questions = loader.load_questions_from_file()
+
+    game = QuizRunner(questions)
+    game.run_quiz()
 
 if __name__ == "__main__":
     main()
